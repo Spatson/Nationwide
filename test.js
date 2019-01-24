ee5ce146b429128e962b40bb1d4276a233f40847
SpencerC <sweetwater55@hotmail.com>
2019-01-23 19:39:36 -0600

    Nationwide

--- test.js
+++ test.js
@@ -4,5 +4,12 @@ $( document ).ready(function(){
     $(".tx:checked").each(function() { total += +this.value; });
     $("#finalTotal").text(total);
   });
+  $('#btn').click(function() {
+    $(".tx:checked").each(function() {
+        var idVal = $(this).attr("item-value");
+        $("#selected").text(idVal);
+        console.log(idVal);
+    });
+});
 
 });
